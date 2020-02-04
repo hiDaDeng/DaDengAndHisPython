9b55af5bfcddc8052b915912932b9ddf6ac34353
Thunderhit <thunderhit@ThunderhitdeMacBookPro.local>
2020-02-04 16:26:28 +0800

    20200204

--- 20190221贝叶斯/code/Eval.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Eval:
-    def __init__(self, pred, gold):
-        self.pred = pred
-        self.gold = gold
-        
-    def Accuracy(self):
-        return np.sum(np.equal(self.pred, self.gold)) / float(len(self.gold))
---
+++ 20200126pandas与seaborn可视化对比/data/business.csv 下载地址.md
@@ -0,0 +1,4 @@
+business.csv 下载地址
+
+链接:https://pan.baidu.com/s/1FWa1Gpmn6ozxH2gIBvU-_Q  密码:1rsw
+
